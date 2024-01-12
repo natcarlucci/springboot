416b64b2f35f48b7c3e1ee504329082bacb7f153
Nathalia <nathalia.carlucci.7@gmail.com>
2024-01-12 01:18:50 -0300

    spring boot

---
+++ src/main/java/com/example/springboot/dtos/ProductRecordDto.java
@@ -0,0 +1,30 @@
+package com.example.springboot.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
